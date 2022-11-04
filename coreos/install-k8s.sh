69b5aeb7ec1df1377c3c8d40e9bf6ccb6c8ee03a
Jordan Conard <jordan.conard3@gmail.com>
2022-11-04 16:23:19 -0400

    Adding R/W directory for FlexVolumes to fix Kubeadm problems.

--- coreos/install-k8s.sh
+++ coreos/install-k8s.sh
@@ -8,4 +8,12 @@ sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
 sudo rpm-ostree install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
-sudo systemctl enable --now kubelet
+sudo systemctl enable --now kubelet containerd.service
+
+if [[ $HOSTNAME == "control1" ]]; then
+  sudo kubeadm init --config clusterconfig.yaml
+fi
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
