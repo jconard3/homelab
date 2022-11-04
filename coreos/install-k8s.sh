e2f0cdec455e537ca0be7d33cdb7f1faf75ed76c
Jordan Conard <jordan.conard3@gmail.com>
2022-11-03 23:01:25 -0400

    fixup! Adding install-k8s.sh script.

--- coreos/install-k8s.sh
+++ coreos/install-k8s.sh
@@ -6,6 +6,6 @@ set -x
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo rpm-ostree install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
 sudo systemctl enable --now kubelet
